6cced8f73b432e376cbb53b1c5a59eef961cf669
Sibasish Satapathy <78403992+sibasish934@users.noreply.github.com>
2022-09-10 21:34:54 +0530

    Create rightView.java

---
+++ src/rightView.java
@@ -0,0 +1,37 @@
+class Solution
+{
+    //Function to return list containing elements of right view of binary tree.
+    ArrayList<Integer> rightView(Node node)
+    {
+       Node root = node;
+       ArrayList<Integer>list = new ArrayList<>();
+       solve(root, list);
+       return list;
+    }
+    public static void solve(Node root, ArrayList<Integer>list){
+        if(root == null){
+            return ;
+        }
+        Queue<Node> q = new LinkedList<>();
+        q.offer(root);
+        
+        while(!q.isEmpty()){
+            int size = q.size();
+            for(int i = 0; i < size; i++){
+                Node temp = q.poll();
+                if(i == size -1){
+                    list.add(temp.data);
+                }
+                
+                if(temp.left!= null){
+                    q.offer(temp.left);
+                }
+                
+                if(temp.right != null){
+                    q.offer(temp.right);
+                }
+            }
+        }
+    }
+}
+
